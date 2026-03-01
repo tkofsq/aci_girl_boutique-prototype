4e10181d5c170896e7ae5ec000762228e8a2d940
tkofsq <bcharlesem@gmail.com>
2026-03-01 14:54:36 +0800

    testing db

--- src/shared/scripts/supabase.js
+++ src/shared/scripts/supabase.js
@@ -1,7 +1,21 @@
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
+const supabase = createClient(
+        'https://moeyonokrqnsrhthkjyd.supabase.co',
+        'sb_publishable_xdY5f0sBT-bzAqOBmCV0Eg_B6Vk3Z_C'
+)
+
+window.addEventListener('DOMContentLoaded', async () => {
+        console.log("DOM is ready")
+        
+        const { data, error } = await supabase.from('Test').select('*')
+        console.log(data)
+})
+
+/*
 export const supabase = createClient(
         'https://moeyonokrqnsrhthkjyd.supabase.co',
         'sb_publishable_xdY5f0sBT-bzAqOBmCV0Eg_B6Vk3Z_C'
 )
+*/
