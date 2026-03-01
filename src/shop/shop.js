ccf815ed8abc891da6cd514881f573f7707a0262
tkofsq <bcharlesem@gmail.com>
2026-03-01 19:50:28 +0800

    shop.js process payment

--- src/shop/shop.js
+++ src/shop/shop.js
@@ -22,5 +22,27 @@ window.addEventListener('DOMContentLoaded', async () => {
 })
 
 const checkout = async(reference, name, phone, address, order) => {
+        const order = order.map(item => ({
+                id: item.id,
+                qty: item.qty
+        }))
         
+        try {
+                const { data, error } = await supabase.rpc('process_order', {
+                        p_reference: reference,
+                        p_phone: phone,
+                        p_name: name,
+                        p_address: address,
+                        p_order: order
+                })
+                if(error) {
+                        console.error("Order Error:", error.message)
+                        alert(`CHeckout failed: ${error.message}`);
+                        return;
+                }
+                alert("Payment submitted! YOur order is pending verification.");
+        } catch(err) {
+                console.error("Unexpected error:", err)
+                alert("An unexpected error occurred. Please try again.")
+        }
 }
