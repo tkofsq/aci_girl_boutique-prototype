c73c55c7d45ea5e290b45fd6a9032e9e595f3005
rosscodes-c <rossdanielmartin@gmail.com>
2026-03-01 18:13:29 +0800

    .

--- src/shop/shop.js
+++ src/shop/shop.js
@@ -4,5 +4,19 @@ import { supabase } from '../shared/scripts/supabase.js'
 window.addEventListener('DOMContentLoaded', async () => {
         const { data, error } = await supabase.from('Item').select('*')
         
-        
+        /*<div class="product-card">
+        <a href="#" class="clickable-card">
+          <div class="img-wrapper">
+            <img src="https://acigirlboutique.com/cdn/shop/files/ED509E1F-C2F2-4D43-A017-B5DF3904876C.jpg?v=1729088231&width=823" alt="Meowy KittyMas Stickers">
+            
+          </div>
+          <h3 class="product-title">CCICCIBOORICCI Meowy KittyMas Stickers</h3>
+          <p class="product-price">
+            <s class="old-price">₱60.00 PHP</s> 
+            <span class="actual-price">From ₱50.00 PHP</span>
+          </p>
+        </a>
+        <button class="btn btn-outline-dark add-to-cart">Choose options</button>
+      </div>*/
+
 })
