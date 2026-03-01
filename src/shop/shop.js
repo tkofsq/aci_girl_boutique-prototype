4ae9603c25f9be2c2a8ddffc9a03ec32302f30b7
tkofsq <bcharlesem@gmail.com>
2026-03-01 17:01:55 +0800

    incomplete shop.js

---
+++ src/shop/shop.js
@@ -0,0 +1,8 @@
+
+import { supabase } from '../shared/scripts/supabase.js'
+
+window.addEventListener('DOMContentLoaded', async () => {
+        const { data, error } = await supabase.from('Item').select('*')
+        
+        
+})
