8aa2150c496446482f710eed635735b388d8d557
khemhy <corpuzjames09@gmail.com>
2026-03-02 09:31:13 +0800

    comments basket

---
+++ src/shop/basket.js
@@ -0,0 +1,55 @@
+/*
+const addToBasket = (_) => {
+        let basket = JSON.parse(localStorage.getItem("basket")) || []
+        
+        basket.push({ id: _ })
+        
+        localStorage.setItem("basket", JSON.stringify(basket))
+}
+*/
+
+// horizontal cards
+
+/*
+import { supabase } from '../shared/scripts/supabase.js'
+
+window.addEventListener('DOMContentLoaded', async () => {
+        let catalog = JSON.parse(localStorage.getItem("catalog"))
+        let data, error;
+        if(!catalog) {
+                ({ data, error } = await supabase.from('Inventory').select('*'))
+                catalog = [[]]
+        }
+        
+        if(error) console.error("Order Error:", error.message)
+        else {
+                for(i = 0, o = 0; i < data.length; i += 4*3, o++) {
+                        catalog[o] = data.slice(i, i+12)
+                }
+                localStorage.setItem("catalog", JSON.stringify(catalog))
+                
+                const dce = document.createElement
+                catalog.forEach((page) => {
+                        const _page = dce("div")
+                        _page.id = `invpg${page}`
+                        _page.class = "grid-0"
+                        page.forEach((product) => {
+                                _page.innerHTML +=
+                                        `<div class="product-card">`+
+                                        `       <a href="#" class="clickable-card">`+
+                                        `               <div class="img-wrapper">`+
+                                        `                       <img src="${product.image}">`+
+                                        `               </div>`+
+                                        `               <h3 class="product-title">${product.name}</h3>`+
+                                        `               <p class="product-price">`+
+                                        `                       <s class="old-price">₱${product.price}</s>`+
+                                        `                       <span class="actual-price">₱${product.final}</span>`+
+                                        `               </p>`+
+                                        `       </a>`+
+                                        `       <button class="btn btn-outline-dark add-to-cart">Add to basket</button>`+
+                                        `</div>`
+                        })
+                })
+        }
+})
+*/
